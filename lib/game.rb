4c9905bd77e3f270917140911db3042fdec73cc1
giamir <giamir.buoncristiani@gmail.com>
2015-12-08 16:05:50 +0000

    use forwardable in Game class to do not violate law of Demeter

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,28 @@
-class Game
+require 'forwardable'
 
-  attr_reader :player1, :player2
+class Game
+  extend Forwardable
+
+  def_delegator :player1, :name, :player1_name
+  def_delegator :player2, :name, :player2_name
+  def_delegator :player1, :hp, :player1_hp
+  def_delegator :player2, :hp, :player2_hp
+
+  attr_reader :players
 
   def initialize(player1, player2)
-    @player1 = player1
-    @player2 = player2
+    @players = [player1, player2]
+  end
+
+  def player1
+    players.first
+  end
+
+  def player2
+    players.last
   end
 
   def attack(player)
     player.receive_damage
   end
-
 end
